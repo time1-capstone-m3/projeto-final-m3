9d42247f109fe04375c08ac42d24420f8a1339e4
Helena J Gomes <heju1958@outlook.com>
2022-08-30 20:44:40 -0300

    feat: footer estrutura/style

---
+++ projeto-final-m3/src/Style/footer.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  background-color: #cecece;
+  width: 100%;
+  height: 3rem;
+  text-align: center;
+  font-size: 13px;
+  p {
+    margin: 0;
+    padding: 1rem;
+    strong {
+      margin: 4px;
+    }
+  }
+`;
---
+++ projeto-final-m3/src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import { FooterContainer } from "../../Style/footer";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <p>
+        Desenvolvido por 
+        <strong>
+          Ângelo Coêlho, Ezequiel Silva, Helena Gomes, José Henrique, Matheus
+          Menezes e Pedro Feitosa.
+        </strong>
+        Kenzie Academy Brasil 2022 - Todos os direitos reservados por fins
+        estudantis.
+      </p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
