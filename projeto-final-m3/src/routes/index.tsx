a302352fa187a91aab0aed43ec5c7c9c3f426cd6
jose_henrique <josehenriqueduarte17@gmail.com>
2022-08-31 15:01:54 -0400

    feat/routes e adição de @types

---
+++ projeto-final-m3/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import {Routes, Route} from 'react-router-dom'
+
+const RoutesMain = () => {
+  return (
+    <Routes>
+      <Route path="/login" />
+      <Route path="/dashboard" />
+      <Route path="/product/id"/>
+      <Route path="/profile"/> 
+    </Routes>
+  )
+}
+
+export default RoutesMain;
