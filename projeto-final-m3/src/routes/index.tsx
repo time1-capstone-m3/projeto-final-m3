111601a7ecb7eb5107d6615e546df11c479efcfe
Ângelo Coêlho <98770132+angelocoelho@users.noreply.github.com>
2022-09-01 19:36:51 -0300

    feat/criação da rota de login

--- projeto-final-m3/src/routes/index.tsx
+++ projeto-final-m3/src/routes/index.tsx
@@ -1,9 +1,10 @@
 import {Routes, Route} from 'react-router-dom'
+import LoginRegister from '../pages/login-cadastro';
 
 const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/login" />
+      <Route path="/login" element={<LoginRegister/>}/>
       <Route path="/dashboard" />
       <Route path="/product/id"/>
       <Route path="/profile"/> 
