5a2ec938be3b37f213996299bac73db626cedc15
Helena J Gomes <heju1958@outlook.com>
2022-09-01 10:47:53 -0300

    fix: import componentes

--- projeto-final-m3/src/App.tsx
+++ projeto-final-m3/src/App.tsx
@@ -1,24 +1,27 @@
 import UserProvider from "./context/UserContext/UserContext";
 import RoutesMain from "./routes";
 import GlobalStyle from "./styles/global";
-import {ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Registration from "./components/Registration";
 import ProductProvider from "./context/productContext";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-     <UserProvider>
-      <ProductProvider>
-        <GlobalStyle />
-        <RoutesMain />
-        <ToastContainer autoClose={1000} />
-        <Header />
-        <Footer />
-        <Card />
-        <Registration/>
-      </ProductProvider>
+      <UserProvider>
+        <ProductProvider>
+          <GlobalStyle />
+          <RoutesMain />
+          <ToastContainer autoClose={1000} />
+          <Header />
+          <Footer />
+          <Card />
+          <Registration />
+        </ProductProvider>
       </UserProvider>
     </div>
   );
