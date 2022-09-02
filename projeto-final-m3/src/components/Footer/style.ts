821a628ac15f75b8491c050d98fbe561aacc322a
Ângelo Coêlho <98770132+angelocoelho@users.noreply.github.com>
2022-09-02 10:03:33 -0300

    fix/correção semântica da tag footer

--- projeto-final-m3/src/components/Footer/style.ts
+++ projeto-final-m3/src/components/Footer/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const FooterContainer = styled.div`
+export const FooterContainer = styled.footer`
   position: fixed;
   bottom: 0;
   background-color: #cecece;
