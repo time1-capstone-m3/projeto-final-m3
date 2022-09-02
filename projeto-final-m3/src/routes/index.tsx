ffe0a1b6de96f45553206256433a740e7cec6f95
Helena J Gomes <heju1958@outlook.com>
2022-09-02 14:37:23 -0300

    fixbugs: regex email

--- projeto-final-m3/src/routes/index.tsx
+++ projeto-final-m3/src/routes/index.tsx
@@ -1,16 +1,15 @@
-import {Routes, Route} from 'react-router-dom'
-import LoginRegister from '../pages/login-cadastro';
+import { Routes, Route } from "react-router-dom";
+import LoginRegister from "../pages/login-cadastro";
 
 const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginRegister/>}/>
+      <Route path="/login" element={<LoginRegister />} />
       <Route path="/dashboard" />
-      <Route path="/product/id"/>
-      <Route path="/profile"/> 
+      <Route path="/product/id" />
+      <Route path="/profile" />
     </Routes>
-  )
-}
+  );
+};
 
 export default RoutesMain;
-
