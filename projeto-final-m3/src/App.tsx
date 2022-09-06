f9802d9625d1150eb3b8d1d485768ceb1cc6b345
Helena J Gomes <102674937+heju1958@users.noreply.github.com>
2022-09-06 08:17:14 -0300

    Revert "Feature/anunciar produtos"

--- projeto-final-m3/src/App.tsx
+++ projeto-final-m3/src/App.tsx
@@ -1,29 +1,7 @@
-import "react-toastify/dist/ReactToastify.css";
-import UserProvider from "./context/UserContext/UserContext";
-import RoutesMain from "./routes";
-import GlobalStyle from "./styles/global";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import ProductProvider from "./context/ProductContext/ProductContext";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <UserProvider>
-        <ProductProvider>
-          <GlobalStyle />
-          <RoutesMain />
-          <ToastContainer
-            autoClose={1500}
-            position={"top-right"}
-            hideProgressBar={false}
-            closeOnClick={true}
-            pauseOnHover={true}
-            draggable={true}
-          />
-        </ProductProvider>
-      </UserProvider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
+
 export default App;
--- projeto-final-m3/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://json-server-time1-m3.herokuapp.com",
-  timeout: 5000,
-});
-
-export default api;
